c59521d03f54409b6064218b4842ba33e09b47f9
Victor Alfredo Macario Enriquez <gokurex39@gmail.com>
2024-01-15 00:08:13 -0600

    first commit

---
+++ Models/Persona.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nuevapractica.Models
+{
+    public partial class Persona
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+    }
+}
---
+++ Models/nuevapracticaContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace nuevapractica.Models
+{
+    public partial class nuevapracticaContext : DbContext
+    {
+        public nuevapracticaContext()
+        {
+        }
+
+        public nuevapracticaContext(DbContextOptions<nuevapracticaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Persona> Personas { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.ToTable("persona");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
